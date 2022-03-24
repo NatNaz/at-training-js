4b11e4158959a30f018edcca503ab8930bc3819a
Natalie.Nazarova <3362282@gmail.com>
2022-03-24 23:51:29 +0300

    задание 5-3, вывод даты, месяца и года

---
+++ js-regexp-3.js
@@ -0,0 +1,7 @@
+let today = new Date;
+let date = today.getDate();
+let month = today.getMonth();
+let year = today.getFullYear();
+console.log ('Date: ' + date);
+console.log ('Month: ' + (month+1));
+console.log ('Year: ' + year);
