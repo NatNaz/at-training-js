3c11b8287b34d7781363ff26b97263b1d5fbbdaa
Natalie.Nazarova <3362282@gmail.com>
2022-03-24 14:09:00 +0300

    задание 1, умножение различных типов

---
+++ js-dataTypes-1.js
@@ -0,0 +1,10 @@
+function addition (arg1,arg2) {
+let sum = arg1 + arg2;
+console.log (typeof(arg1) + ' + ' + typeof(arg2) + ' = ' + typeof(sum) + '\n ' + arg1 + ' + ' + arg2 + ' = ' + sum);
+}
+addition ('secret ',true);
+addition (false, ' secret');
+addition (19, true);
+addition (false, 22);
+addition (5,' little monkeys');
+addition ('Jackson ', 5);
