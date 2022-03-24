dbd684ac94cee18cd524ff4969eb114171188c9a
Natalie.Nazarova <3362282@gmail.com>
2022-03-24 14:34:37 +0300

    задание 3-1

---
+++ js-arroys-1.js
@@ -0,0 +1,2 @@
+let movies = ["Marvel's The Avengers", "Iron Man", "Thor", "Captain America: The Winter Soldier" , "Doctor Strange" , "Avengers: Infinity War", "Eternals" , "Black Widow", ];
+movies.forEach(console.log);
