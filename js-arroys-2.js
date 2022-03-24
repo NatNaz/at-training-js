2f81abfaf673b02cf1de96e7bad9df13eccab798
Natalie.Nazarova <3362282@gmail.com>
2022-03-24 14:34:54 +0300

    задание 3-2

---
+++ js-arroys-2.js
@@ -0,0 +1,4 @@
+let cars = ['Volkswagen Auto Group (VAG)', 'Toyota Motor Corp.', 'Компания Honda.', 'Концерн Groupe PSA.', 'Альянс Renault-Nissan-Mitsubishi.', 'Концерн General Motors.', 'Компания Ford.', 'Концерн Fiat.',];
+let strCars = cars.join('; ');
+let newCars = strCars.split('; ');
+newCars;
