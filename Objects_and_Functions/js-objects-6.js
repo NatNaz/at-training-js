17d74633dfa58e6f9aaaeb18cfc75d7cc5e20902
NatNaz <96595238+NatNaz@users.noreply.github.com>
2022-03-29 00:18:08 +0300

    Update js-objects-6.js

--- Objects_and_Functions/js-objects-6.js
+++ Objects_and_Functions/js-objects-6.js
@@ -1,3 +1,8 @@
 function defineTypeOfAgument(agument) {
     console.log (typeof(agument));
 }
+defineTypeOfAgument('agument');
+defineTypeOfAgument(24);
+defineTypeOfAgument(true);
+defineTypeOfAgument(0);
+defineTypeOfAgument(null);
