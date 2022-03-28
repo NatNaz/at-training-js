98575aa6f52e990c923287231ddc8a20fe79fa5e
NatNaz <96595238+NatNaz@users.noreply.github.com>
2022-03-28 22:47:48 +0300

    Delete js-arrays-2.js

--- js-arrays-2.js
+++
@@ -1,4 +0,0 @@
-let cars = ['Volkswagen Auto Group (VAG)', 'Toyota Motor Corp.', 'Компания Honda.', 'Концерн Groupe PSA.', 'Альянс Renault-Nissan-Mitsubishi.', 'Концерн General Motors.', 'Компания Ford.', 'Концерн Fiat.',];
-let strCars = cars.join('; ');
-let newCars = strCars.split('; ');
-newCars;
