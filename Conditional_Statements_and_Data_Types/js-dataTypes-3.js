2cee7060e4ab05e6b18913a54bb99e319b6608b0
Natalie.Nazarova <3362282@gmail.com>
2022-03-28 21:31:28 +0300

    Data Types new folder

---
+++ Conditional_Statements_and_Data_Types/js-dataTypes-3.js
@@ -0,0 +1,10 @@
+function dividion (arg1,arg2) {
+    let result = arg1 / arg2;
+    console.log (typeof(arg1) + ' / ' + typeof(arg2) + ' = ' + typeof(result) + '\n ' + arg1 + ' / ' + arg2 + ' = ' + result);
+    }
+    dividion ('secret ',true);
+    dividion (false, ' secret');
+    dividion (19, true);
+    dividion (false, 22);
+    dividion (5,' little monkeys');
+    dividion ('Jackson ', 5);
