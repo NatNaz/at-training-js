b4493f0d2bc4b8b3a266f5dafbc430761a8b7c32
NatNaz <96595238+NatNaz@users.noreply.github.com>
2022-03-28 15:05:26 +0300

    Update monkeysOnTheBed.js

--- monkeysOnTheBed.js
+++ monkeysOnTheBed.js
@@ -1,9 +1,6 @@
 function monkeysOnTheBed (monkeysNumber) {
     for (let i=monkeysNumber; i>0; i--) {
-        console.log( i + ' little monkeys are jumping on the bed 
-        \n One felt off and bumped his hed! 
-        \n Mother call the Doctor and the Doctor said: 
-        \n "No more monkeys jumping on the Bed!"')
+        console.log( i + ' little monkeys are jumping on the bed \n One felt off and bumped his head! \n Mother call the Doctor and the Doctor said: \n "No more monkeys jumping on the Bed!"')
     }
     console.log ('There\'re no more monkeys on the bed!');
 }
