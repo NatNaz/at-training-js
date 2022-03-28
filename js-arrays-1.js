9ad64b469b6ea031c083767e7dbd10a5a4f369e7
NatNaz <96595238+NatNaz@users.noreply.github.com>
2022-03-28 22:47:11 +0300

    Delete js-arrays-1.js

--- js-arrays-1.js
+++
@@ -1,2 +0,0 @@
-let movies = ["Marvel's The Avengers", "Iron Man", "Thor", "Captain America: The Winter Soldier" , "Doctor Strange" , "Avengers: Infinity War", "Eternals" , "Black Widow", ];
-movies.forEach(console.log);
