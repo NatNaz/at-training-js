21f3c8ecfcb9e690c9695f0c9e86e9223505d47e
Natalie.Nazarova <3362282@gmail.com>
2022-05-02 23:11:53 +0300

    finish

--- Conditional_Statements_and_Data_Types/js-dataTypes-1.js
+++ Conditional_Statements_and_Data_Types/js-dataTypes-1.js
@@ -1,3 +1,4 @@
+// Выполнить сложение различных типов(string+boolean, string+number, number+boolean)
 function addition (arg1,arg2) {
 let sum = arg1 + arg2;
 console.log (typeof(arg1) + ' + ' + typeof(arg2) + ' = ' + typeof(sum) + '\n ' + arg1 + ' + ' + arg2 + ' = ' + sum);
