fdbef5e4f8835c6e6ffc6f1c399059eb6553f2b4
Natalie.Nazarova <3362282@gmail.com>
2022-05-02 23:12:41 +0300

    finish

--- Objects_and_Functions/js-objects-1-2-3.js
+++ Objects_and_Functions/js-objects-1-2-3.js
@@ -1,7 +1,9 @@
+// Создать объект car, добавить к нему свойство color со значением 'черный'
+// Изменить свойство color объекта car на 'зеленый'
+// В объект car добавить свойство power, которое является функцией и выводит в консоль мощность двигателя
 let car = {};
-let enginePower = 250;
 car.color = 'чёрный';
 car.color = 'зелёный';
-car.power = function () {
-    console.log('The engine power is ' + enginePower);
-}
+car.power = (carPower) => console.log(`The engine power is ${carPower}`);
+console.log(car);
+car.power(100);
