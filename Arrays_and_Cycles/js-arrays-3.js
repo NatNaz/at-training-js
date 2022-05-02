86e1244b8fcb4a1ded6fa95884c347a4d94e42ed
Natalie.Nazarova <3362282@gmail.com>
2022-05-02 23:10:57 +0300

    finish

--- Arrays_and_Cycles/js-arrays-3.js
+++ Arrays_and_Cycles/js-arrays-3.js
@@ -1,3 +1,4 @@
+// Дан массив имен ваших знакомых, добавить к каждому элементу массива слова hello
 let friends = ['Mary', 'Kate', 'John', 'Nelly', 'Adam', 'Lex',];
 let greetingFriends = friends.map(x => {return 'Hello ' + x});
-greetingFriends;
+console.log(greetingFriends);
