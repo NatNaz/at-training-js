f706d94207d1c16fb84e77c594bb43e2a81dfc42
Natalie.Nazarova <3362282@gmail.com>
2022-05-02 23:12:49 +0300

    finish

--- Objects_and_Functions/js-objects-4.js
+++ Objects_and_Functions/js-objects-4.js
@@ -1,3 +1,5 @@
+/* На склад принимают груши и яблоки, напишите функцию,
+которая возвращает результат сложения количества принятых груш и яблок */
 function countApplesAndPears(apples, pears) {
     return(Number(apples) + Number(pears));
 }
