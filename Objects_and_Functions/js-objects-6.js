599f7fe09c16058bce4c0235cb9f4d6f3dc9dd01
Natalie.Nazarova <3362282@gmail.com>
2022-05-02 23:13:13 +0300

    finish

--- Objects_and_Functions/js-objects-6.js
+++ Objects_and_Functions/js-objects-6.js
@@ -1,3 +1,9 @@
-function defineTypeOfAgument(agument) {
-    console.log (typeof(agument));
+// Напишите функцию вычисления типа аргумента и вывод типа в консоль
+function defineTypeOfArgument(argument) {
+    console.log (typeof(argument));
 }
+defineTypeOfArgument('string');
+defineTypeOfArgument(23);
+defineTypeOfArgument(true);
+defineTypeOfArgument(defineTypeOfArgument);
+defineTypeOfArgument(undefined);
