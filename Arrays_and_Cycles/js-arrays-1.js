35e7cc1d7416797f26660028be33f728944a10ec
Natalie <3362282@gmail.com>
2022-06-20 00:08:20 +0300

    add unit-5 task-1

---
+++ Arrays_and_Cycles/js-arrays-1.js
@@ -0,0 +1,18 @@
+/* Дан массив состоящий из названий фильмов, выполните перебор
+массива с выводом в консоль названия каждого фильма*/
+let movies = [
+  "Marvel's The Avengers",
+  "Iron Man",
+  "Thor",
+  "Captain America: The Winter Soldier",
+  "Doctor Strange",
+  "Avengers: Infinity War",
+  "Eternals",
+  "Black Widow",
+];
+function outputMovies() {
+  for (let i = 0; i < movies.length; i++) {
+    console.log(movies[i]);
+  }
+}
+outputMovies();
