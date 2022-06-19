2b891651decb8167ee2aa0f9b418a69ce37f0751
Natalie <3362282@gmail.com>
2022-06-20 00:19:14 +0300

    add unit-6 task-1

---
+++ Prototype_and_Inheritance/js-proto-1.js
@@ -0,0 +1,10 @@
+/* Создать 2 объекта: animal и cat, 
+объект animal добавить свойство move, 
+объект cat должен наследовать свойство move*/
+let animal = {
+  move: true,
+};
+let cat = {
+  __proto__: animal,
+};
+console.log(cat.move);
