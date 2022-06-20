b9979950c9b8e12b0622ff00a0b645be7315a7f1
Natalie <3362282@gmail.com>
2022-06-21 00:05:16 +0300

    fix validation in unit-4 task-7

--- Objects_And_Functions/js-objects-7.js
+++ Objects_And_Functions/js-objects-7.js
@@ -1,23 +1,28 @@
 // Напишите функцию, которая определяет является ли число простым или нет
 
 function definePrime(number) {
-  let numberToCheck = {
-    value: number,
-    prime: true,
-  };
-  check: for (let i = 2; i <= Math.floor(Math.sqrt(number)); i++) {
-    let rest = number % i;
-    if (rest !== 0) {
-      continue check;
-    } else {
-      numberToCheck.prime = false;
-      console.log(
-        "The number is composite! " + number + " divides by " + i + "."
-      );
+  if (number <= 1) {
+    console.log(number + " can't be defined as prime.");
+  } else {
+    let numberToCheck = {
+      value: number,
+      prime: true,
+    };
+
+    check: for (let i = 2; i <= Math.floor(Math.sqrt(number)); i++) {
+      let rest = number % i;
+      if (rest !== 0) {
+        continue check;
+      } else {
+        numberToCheck.prime = false;
+        console.log(
+          "The number is composite! " + number + " divides by " + i + "."
+        );
+      }
     }
-  }
-  if (numberToCheck.prime != false) {
-    console.log("The number is prime!");
+    if (numberToCheck.prime !== false) {
+      console.log("The number is prime!");
+    }
   }
 }
 definePrime(23);
