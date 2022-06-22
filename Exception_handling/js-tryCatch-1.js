93dbf3ad98c05ba8ec3363eb6ea698782f992e94
Natalie <3362282@gmail.com>
2022-06-22 22:47:50 +0300

    4th try unit-8 task-1

--- Exception_handling/js-tryCatch-1.js
+++ Exception_handling/js-tryCatch-1.js
@@ -1,25 +1,27 @@
 /* В try catch конструкцию завернуть код: console.log(a), let a = 3. 
 И вывести ошибку – ‘let перед использованием нужно объявить’. 
 При выполнении 1/0  выводить ошибку 'на ноль делить нельзя' */
+try {
+  console.log(a);
+  let a = 3;
+} catch (e) {
+  if ((e.name = "ReferenceError")) {
+    console.log("let перед использованием нужно объявить");
+  } else {
+    throw e;
+  }
+}
+
 function exercise() {
-  try {
-    console.log(a);
-    let a = 3;
-    let b = 0;
-    console.log(1 / b);
-  } catch (e) {
-    if ((e.name = "ReferenceError")) {
-      console.log("let перед использованием нужно объявить");
-    } else {
-      throw e;
-    }
+  let b = 1 / 0;
+  if (b === Infinity) {
+    throw new TypeError("Infinity");
+  } else {
+    console.log(b);
   }
 }
 try {
   exercise();
-  if (b === 0) {
-    throw new TypeError("Infinity");
-  }
 } catch (e) {
   if (e.name == "TypeError") {
     console.log("На ноль делить нельзя");
